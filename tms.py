d3bbaf042f64ef99823c6ad2ed931224cee0e0f7
shahlaibadzadeh <shahlaibadzadeh@gmail.com>
2022-09-20 13:22:57 +0400

    Taking command from user

--- tms.py
+++ tms.py
@@ -4,3 +4,18 @@ class Student:
 
     def __init__(self):
         pass
+
+
+
+print("Welcome to Student Management System!")
+print("""You can choose one of these functions:
+List students
+Add student
+Search student
+Assign grade
+Add and view announcement
+Delete student""")
+
+command = ''
+while command == "Log out":
+    command = input("Enter your command: ").lower()
