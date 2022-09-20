83da4a50bb528d0276434bac20df64d97f31d2c9
shahlaibadzadeh <shahlaibadzadeh@gmail.com>
2022-09-20 15:39:00 +0400

    Function to add student

--- tms.py
+++ tms.py
@@ -4,7 +4,15 @@ class Student:
 
     def __init__(self):
         pass
-
+    
+    def add_student(self):
+        self.name = input("Enter student's name: ").capitalize()
+        self.surname = input("Enter student's surname: ").capitalize()
+        self.fullname = self.name + " " + self.surname
+        self.email = self.name.lower() + self.surname.lower() + "@gmail.com"
+        if self.email in Student.students:
+            self.email = self.name.lower() + self.surname.lower() + "2@gmail.com"
+        Student.students.append(dict({"fullname": self.fullname, "email": self.email}))
 
 
 print("Welcome to Student Management System!")
@@ -16,6 +24,18 @@ Assign grade
 Add and view announcement
 Delete student""")
 
-command = ''
+command = ""
 while command == "Log out":
     command = input("Enter your command: ").lower()
+    if command == "add student":
+        std = Student()
+        std.add_student()
+        while input("Do you want to add new student? Y/N ").lower() == "y":
+            std = Student()
+            std.add_student()
+        else:
+            print("Students were added successfully!")
+
+    
+
+    
